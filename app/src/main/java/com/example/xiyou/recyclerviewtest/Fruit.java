639cac3de746a198a464620c3f990f644c49144f
unknown <you@example.com>
2018-04-12 19:57:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiyou/recyclerviewtest/Fruit.java
@@ -0,0 +1,22 @@
+package com.example.xiyou.recyclerviewtest;
+
+/**
+ * Created by XiYou on 2018/4/8.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+    public Fruit(String name, int imageId){
+        this.name=name;
+        this.imageId=imageId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getImageId(){
+        return imageId;
+    }
+}
